7d6375578601a459689a9d8c42d09a26b1d0b8d3
Afton Flynn <afton@Aftons-MBP.hitronhub.home>
2023-02-25 16:27:17 -0700

    Final API data pull structure'

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Example fetch using pokemonapi.co
-document.querySelector('input').addEventListener('keydown', getFetch)
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
@@ -10,9 +9,6 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        let commonNames = data.data.attributes.name;
-        let capitalizedCommonNames = commonNames.split(' ').map(word => word.charAt(0).       toUpperCase() + word.slice(1)).join(' ');
-        document.querySelector('h2').innerText = capitalizedCommonNames;
         document.querySelector('#icon').innerHTML = data.data.attributes.svg_icon
         document.querySelector('#sowing').innerHTML = 'Sowing Method: ' + data.data.attributes.sowing_method
         document.querySelector('#sun').innerHTML = 'Requires: ' + data.data.attributes.sun_requirements
